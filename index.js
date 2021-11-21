2d9326e7df4299eebae43289933844aaef76efa9
Umar Khan <umarkhan167@outlook.com>
2021-11-21 12:13:33 -0500

    Completed ISS Spotter 3: Array of risetimes successfully outputted.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // index.js
 //const { fetchMyIP } = require('./iss');
 // const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -18,3 +19,11 @@
 //   }
 //   console.log('Here is your coordinate location:' , data);
 // })
+
+fetchISSFlyOverTimes({latitude: 43.8864, longitude: -78.9742}, (error, response) =>{
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('Here are the fly over times for your coordinates:' , response);
+})
--- iss.js
+++ iss.js
@@ -61,9 +61,46 @@ const fetchCoordsByIP = function(ip, callback) {
 };
 
 
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+ const fetchISSFlyOverTimes = function(coords, callback) {
+  // https://iss-pass.herokuapp.com/json/?lat=43.8864&lon=-78.9742
+  const url = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+  request(url, (error, response, body) => {
+     // error can be set if invalid domain, user is offline, etc.
+     if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const dataObj = JSON.parse(body);
+    const responseArr = dataObj.response;
+    
+    if (!dataObj) {
+      callback(`${responseArr} not found.`, null);
+    } else {
+      callback(null, responseArr);
+    }
+  })
+
+};
 
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
 
 
 
