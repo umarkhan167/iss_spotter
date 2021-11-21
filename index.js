1988812f3233b3048514e85d62aba6b569ad613e
Umar Khan <umarkhan167@outlook.com>
2021-11-21 12:29:18 -0500

    Completed ISS Spotter 4: Prints the 5 next passing times.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // index.js
 //const { fetchMyIP } = require('./iss');
 // const { fetchCoordsByIP } = require('./iss');
-const { fetchISSFlyOverTimes } = require('./iss');
+// const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -20,10 +21,28 @@ const { fetchISSFlyOverTimes } = require('./iss');
 //   console.log('Here is your coordinate location:' , data);
 // })
 
-fetchISSFlyOverTimes({latitude: 43.8864, longitude: -78.9742}, (error, response) =>{
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
+// fetchISSFlyOverTimes({latitude: 43.8864, longitude: -78.9742}, (error, response) =>{
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('Here are the fly over times for your coordinates:' , response);
+// })
+
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
-  console.log('Here are the fly over times for your coordinates:' , response);
-})
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
